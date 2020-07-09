b8a5638be309c0a92fb8e0d761dbe85ed5aa90cb
Magdalena Pietruszewska <magdalena-pietruszewska@wp.pl>
2020-07-09 12:27:28 +0200

    Updates

---
+++ precache-manifest.19df4c59705b3d86d792f393527c1e1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fc73d7dcfe4403b673d0a9138d5438",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "b0815f08c07e98016fc4",
+    "url": "/ToDoList/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8d23f6444f04278dd1a7",
+    "url": "/ToDoList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b0815f08c07e98016fc4",
+    "url": "/ToDoList/static/js/2.e9c2f1a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.e9c2f1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d23f6444f04278dd1a7",
+    "url": "/ToDoList/static/js/main.65bae2d0.chunk.js"
+  },
+  {
+    "revision": "d5b7c62caf7374bf2c1c",
+    "url": "/ToDoList/static/js/runtime-main.ef6521ad.js"
+  }
+]);
